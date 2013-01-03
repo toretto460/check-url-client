60485a8703646f7f34eaab92b816b0125b46d5be
toretto460 <toretto460@gmail.com>
2013-01-03 22:44:57 +0100

    Dependencies

--- app.js
+++ app.js
@@ -1,13 +1,38 @@
 var flatiron = require('flatiron'),
+		redis = require('redis'),
     path = require('path'),
-    app = flatiron.app;
+    app = flatiron.app,
+    io = require('socket.io');    
 
 app.config.file({ file: path.join(__dirname, 'config', 'config.json') });
 
 app.use(flatiron.plugins.http);
+app.use(socketful);
 
 app.router.get('/', function () {
   this.res.json({ 'hello': 'world' })
 });
 
 app.start(3000);
+io.listen(app);
+
+io.sockets.on('connection', function (socket) {
+
+	console.log('connected');
+
+		var db = redis.createClient(redisConf.port, redisConf.host);
+		db.auth(redisConf.psw);
+		db.subscribe(queue);
+		
+
+
+		db.on('message',function(channel, message){
+			socket.emit('message', message);
+		});
+
+		socket.on('disconnect', function(){
+			db.unsubscribe();
+			db.end();
+		});	
+
+});
