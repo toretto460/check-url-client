bfb2c11c896b8f7dce74488d9afaa1554718169b
toretto460 <toretto460@gmail.com>
2013-02-20 23:34:01 +0100

    themed with bootstrap

--- public/admin.js
+++ public/admin.js
@@ -2,10 +2,23 @@ function update( msg ) {
   msg = JSON.parse(msg);
   var el = "#" + msg.id;
   if( $(el).length ){
-    $(el).val(msg.url + ": " + msg.code);
-  } else {
-    $("#list").append('<li id="' + msg.id + '">' + msg.url + ': ' + msg.code + '</li>')
+    $(el).remove(); 
   }
+
+  label_class = mapCodeToLabel(msg.code);
+  $("#list").append('<li id="' + msg.id + '"><span class="url-name">' + msg.url + ':</span> <span class="label label-' + label_class + '">'+ msg.code +'</span></li>')
+
+}
+
+function mapCodeToLabel(code) {
+      if ( code == 200 ) {
+      label_class = 'success';
+    } else if (code == 500 ) {
+      label_class = 'error';
+    } else {
+      label_class = 'warning';
+    }
+    return label_class;
 }
 
 $(document).ready(function () {
