fc89b9e6b1018c853fbed74307027d6823c9ec45
toretto460 <toretto460@gmail.com>
2013-02-21 20:58:09 +0100

    Moved configuration in json file

--- app/app.js
+++ app/app.js
@@ -1,11 +1,12 @@
 var app = require('express')(),
-    path = require('path');    
+    path = require('path'),
+    settings = require('../config/config.json');
 
 var port = process.env.PORT || 8080;
 
 var server = app.listen(port);
 var io = require('socket.io').listen(server);
-var queue = "check-url";
+
 
 //Look for static files in "/public" folder
 app.configure(function(){
@@ -16,7 +17,7 @@ io.sockets.on('connection', function (socket) {
 
 	var redis = require('../lib/redistogo');
 	sub = redis.createClient();
-	sub.subscribe(queue);
+	sub.subscribe(settings.queue.name);
 	
 	sub.on('message',function(channel, message){
 		socket.emit('message', message);
