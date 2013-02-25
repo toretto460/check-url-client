89d1e4bbf31e270ee8c5d68f315ae5064fceb404
toretto460 <toretto460@gmail.com>
2013-02-25 23:47:18 +0100

    Added render engine

--- app/app.js
+++ app/app.js
@@ -2,16 +2,35 @@ var app = require('express')(),
     path = require('path'),
     settings = require('../config/config.json');
 
-var port = process.env.PORT || 8080;
-
-var server = app.listen(port);
-var io = require('socket.io').listen(server);
-var queue = settings.queue_subscribe.name;
-
 //Look for static files in "/public" folder
 app.configure(function(){
-	app.use(require('express').static(__dirname + '/../public'));	
+	app.set('port', process.env.PORT || 8080);
+	app.use(require('express').static(__dirname + '/../public'));
+	app.engine('html', require('hbs').__express);
+	app.set('view engine', 'html');
+	app.set('views', __dirname + '/../views');
 });
+
+// POST /url/new
+app.get('/url/new', function(req, res){
+  // get a form for insert new url
+  var data = {
+  	url: "/url/add"
+  }
+  res.render('forms/url-form.html', data);
+});
+
+// POST /url/add
+app.post('/url/add', function(req, res){
+	//save url with id, name and timeout value
+	//seve a key on redis and publish a start message on a queue
+	//....write the code
+	console.log('implement POST: /url/add');
+});
+
+var server = app.listen(app.get('port'));
+var io = require('socket.io').listen(server);
+var queue = settings.queue_subscribe.name;
 
 io.sockets.on('connection', function (socket) {
 
